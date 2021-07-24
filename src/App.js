618cdf9b8524fc132ec9c9bb1d1df1231305cb31
patryk <patryk.r.nowak@gmail.com>
2021-07-24 10:09:09 +0100

    Test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [bColor, setBColor] = useState("red");
+
+  const newColor = bColor === "red" ? "blue" : "red";
+
+  const handler = () => setBColor(newColor);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={handler} style={{ backgroundColor: bColor }}>Change to {newColor}</button>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,37 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, fireEvent } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+// test("renders learn react link", () => {
+//   // render(<App />);
+//   // //  by role - always try to use selector accessible by screen readers etc.
+//   // const linkElement = screen.getByRole('link',{name: /learn react/i});
+//   // expect(linkElement).toBeInTheDocument();
+// });
+test("button has correct initial color", () => {
+  // element which we are going to render
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  // global access to rendered dom
+  // We will find it by rol
+  const colorButton = screen.getByRole("button", { name: "Change to blue" });
+
+  // expecting results - color in that case
+  expect(colorButton).toHaveStyle({ backgroundColor: "red" });
+
+  //click button - fireEvent - allowing us to interact with the dom
+  fireEvent.click(colorButton);
+  // what do we expect after?
+  expect(colorButton).toHaveStyle({ backgroundColor: 'blue' });
+
+  // expectation: button text = "Changed to red"
+  expect(colorButton.textContent).toBe("Change to red");
 });
+
+//  no needed because we are finding it by initial color - by text
+// test("button has correct initial text", () => {
+
+// });
+
+// test("button turns blue when clicked", () => {
+//   render(<App />);
+//   const colorButton = screen.getByRole("button", { name: "Change to blue" });
+// });
