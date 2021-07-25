49ce41a19268e891caf8b23994d51bd53a64dd26
patryk <patryk.r.nowak@gmail.com>
2021-07-25 17:55:58 +0100

    end of section

--- sundaes-on-demand/src/pages/entry/tests/OrderEntry.test.jsx
+++ sundaes-on-demand/src/pages/entry/tests/OrderEntry.test.jsx
@@ -1,24 +1,45 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import { rest } from "msw";
 import OrderEntry from "../OrderEntry";
 import { server } from "../../../mocks/server";
 
+// to run only one test to do: test.only()
+// to run all and skip something test.skip()
+
 test("handles errors for scoops and toppings", async () => {
-  // we are reseting handlers and prepering new for a sake of test
+  // we are resetting handlers and preparing new for a sake of test
   server.resetHandlers(
+    //       'http://localhost:3030/scoops'
+
     rest.get("http://localhost:3030/scoops", (req, res, ctx) =>
-      res(ctx.status500)
+      res(ctx.status(500))
     ),
-    rest.get("http://localhost:3030/scoops", (req, res, ctx) => {
-      res(ctx.status500);
+    // rest.get("http://localhost:3030/scoops", (req, res, ctx) =>
+    //   res(ctx.status(500))
+    // ),
+    //       'http://localhost:3030/toppings'
+
+    rest.get("http://localhost:3030/toppings", (req, res, ctx) => {
+      // the same like in express You have to return (exit) from middleware OMG! :>
+      return res(ctx.status(500));
     })
+
+    // rest.get("http://localhost:3030/toppings", (req, res, ctx) =>
+    //   res(ctx.status(500))
+    // )
   );
 
   render(<OrderEntry />);
 
-  const alerts = await screen.findAllByRole("alert", {
-    name: "An unexpected error ocurred. Please try again later.",
-  });
+  //   in this configuration it is not going to await all just is going to await till first
+  // error is going to be back!
+  //   const alerts = await screen.findAllByRole("alert", {
+  //     name: "An unexpected error ocurred. Please try again later.",
+  //   });
 
-  expect(alerts).toHaveLength(2);
+  //waiting for all of two errors! tricky thing :)
+  await waitFor(async () => {
+    const alerts = await screen.findAllByRole("alert");
+    expect(alerts).toHaveLength(2);
+  });
 });
